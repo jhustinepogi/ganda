f49c34b4b834e0dcd24ed2ee0e7593560e9529f5
jhustinepogi <145885021+jhustinepogi@users.noreply.github.com>
2024-02-07 21:42:19 +0800

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ noButton.addEventListener("click", function () {
 });
 
 function handleYesClick() {
-  titleElement.innerHTML = "Yayyy!! :3";
+  titleElement.innerHTML = "Yayyy!! Iloveyouuu :3";
   buttonsContainer.classList.add("hidden");
   changeImage("yes");
 }
